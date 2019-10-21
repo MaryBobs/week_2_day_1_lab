89591deddd5ca5181b75886e5dae2c06ddfba62c
MaryBobs <55357115+MaryBobs@users.noreply.github.com>
2019-10-21 13:34:26 +0100

    section A first four tests passing

---
+++ day_1/specs/student_spec.rb
@@ -0,0 +1,29 @@
+require("minitest/autorun")
+require("minitest/rg")
+require_relative("../student.rb")
+
+class TestStudent < MiniTest::Test
+
+def test_student_name
+  student = Student.new("Fred", "E35")
+  assert_equal("Fred", student.get_name)
+end
+
+def test_student_cohort
+  student = Student.new("Fred", "E35")
+  assert_equal("E35", student.get_cohort)
+end
+
+def test_set_student_name
+  student = Student.new("Fred", "E35")
+  student.set_name("Joe")
+  assert_equal("Joe", student.get_name)
+end
+
+def test_set_cohort_name
+  student = Student.new("Fred", "E35")
+  student.set_cohort("G07")
+  assert_equal("G07", student.get_cohort)
+end
+
+end
