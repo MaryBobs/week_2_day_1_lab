2780f91f35a201a584fc4581bea054b7862c902e
MaryBobs <55357115+MaryBobs@users.noreply.github.com>
2019-10-21 15:46:04 +0100

    Finished MVP

--- day_1/specs/sports_team.rb
+++ day_1/specs/sports_team.rb
@@ -5,36 +5,42 @@ require_relative("../sports_team.rb")
 class TestSportsTeam < MiniTest::Test
 
 def test_get_team_name
-  team_name = SportsTeam.new("Ducks United", ["Fred", "Joe"], "Sandy")
+  team_name = SportsTeam.new("Ducks United", ["Fred", "Joe"], "Sandy", 0)
   assert_equal("Ducks United", team_name.team_name)
 end
 
 def test_get_players
-  players = SportsTeam.new("Ducks United", ["Fred", "Joe"], "Sandy")
+  players = SportsTeam.new("Ducks United", ["Fred", "Joe"], "Sandy", 0)
   assert_equal(["Fred", "Joe"], players.players)
 end
 
 def test_get_coach
-  coach = SportsTeam.new("Ducks United", ["Fred", "Joe"], "Sandy")
+  coach = SportsTeam.new("Ducks United", ["Fred", "Joe"], "Sandy", 0)
   assert_equal("Sandy", coach.coach)
 end
 
 def test_update_coach
-  coach = SportsTeam.new("Ducks United", ["Fred", "Joe"], "Sandy")
+  coach = SportsTeam.new("Ducks United", ["Fred", "Joe"], "Sandy", 0)
   coach.coach = "Eugene"
   assert_equal("Eugene", coach.coach)
 end
 
 def test_add_new_player
-    players = SportsTeam.new("Ducks United", ["Fred", "Joe"], "Sandy")
+    players = SportsTeam.new("Ducks United", ["Fred", "Joe"], "Sandy", 0)
     players.add_new_player("Pete")
     assert_equal(3, players.players.count)
 end
 
 def test_find_player
-  players = SportsTeam.new("Ducks United", ["Fred", "Joe"], "Sandy")
+  players = SportsTeam.new("Ducks United", ["Fred", "Joe"], "Sandy", 0)
   players.find_player("Fred")
   assert_equal('Fred', players.players[0])
+end
+
+def test_win_or_lose
+  points = SportsTeam.new("Ducks United", ["Fred", "Joe"], "Sandy", 0)
+  points.win_or_lose("Win")
+  assert_equal(3, points.points)
 end
 
 
--- day_1/sports_team.rb
+++ day_1/sports_team.rb
@@ -2,12 +2,13 @@ class SportsTeam
 
   #attr_reader :team_name, :players, :coach
   #attr_writer :team_name, :players, :coach
-  attr_accessor :team_name, :players, :coach
+  attr_accessor :team_name, :players, :coach, :points
 
-def initialize(team_name, players, coach)
+def initialize(team_name, players, coach, points)
   @team_name = team_name
   @players = players
   @coach = coach
+  @points = points
 end
 
 def add_new_player(name)
@@ -18,6 +19,12 @@ def find_player(name)
     if @players.include?(name)
       return true
     end
+end
+
+def win_or_lose(result)
+  if result == "Win"
+    @points += 3
+  end
 end
 
 end
